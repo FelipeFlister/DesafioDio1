88ae9d7c2aa81e4753c16aed075cec93b6587ffd
FelipeFlister <felipeflister@gmail.com>
2023-12-10 22:09:09 -0300

    desafio-logica-dio

---
+++ .vscode/Meus Projetos/Teste.js
@@ -0,0 +1,25 @@
+let Pkm = [
+["Pikachu", 1]
+]
+console.log(Pkm[0][0] + "nível" + Pkm[0][1])
+let Teste = true && false
+console.log(Teste)
+
+let Pkm2 = "Absol"
+
+switch(Pkm2){
+    case ("Pikachu"):
+        console.log("Achei um Pikachu!")
+        break
+    default:
+        console.log("Achei alguma coisa")
+}
+let item = false
+switch(item){
+case(item = false):
+console.log(0) 
+}
+
+for(let i = 0; i <= 999999999999999999999999999999999999999999999999999999; i++){
+    console.log(i)
+}
