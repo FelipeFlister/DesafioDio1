98fcc4ba010736589f727a405eb973d0857d646d
FelipeFlister <153518854+FelipeFlister@users.noreply.github.com>
2023-12-14 15:43:11 -0300

    Update DesafioSecreto.js

--- Projetos DIO/PortugueseProjects/DesafioSecreto.js
+++ Projetos DIO/PortugueseProjects/DesafioSecreto.js
@@ -1,38 +1,49 @@
-class PokeCenter{
-    constructor(PN, PT, PL, PH, PS){
-        this.PN = PN
-        this.PT = PT
-        this.PL = PL
-        this.PH = PH
+/* NDP: Nome do Pokémon
+   TDP: Tipo do Pokémon
+   NIP: Nível do Pokémon
+   PS: Pontos de Saúde
+   SDP: Sexo do Pokémom
+*/
+
+//--------------------------------A Classe do Centro Pokémon---------------------------------------
+class CentroPokemon{
+    constructor(NDP, TDP, NIP, PS, SDP){
+        this.NDP = NDP
+        this.TDP = TDP
+        this.NIP = NIP
         this.PS = PS
+        this.SDP = SDP
     }
-    printPokeInfo(){
-        console.log(`${this.PN}  foi cadastrado com sucesso! `)
+    ImprimirNotas(){
+        console.log(`${this.NDP}  foi cadastrado com sucesso! `)
         console.log(`------------------------------------------------`)
         console.log(`Informações sobre seu Pokémon:`)
-        console.log(`Seu nome é: ${this.PN}.`)
-        console.log(`Seu TYPE é: ${this.PT}.`)
-        console.log(`Seu LEVEL é: ${this.PL}.`)
-        console.log(`Seu HP é: ${this.PH}.`)
-        console.log(`Seu Sexo é: ${this.PS}.`)
+        console.log(`Seu nome é: ${this.NDP}.`)
+        console.log(`Seu TYPE é: ${this.TDP}.`)
+        console.log(`Seu LEVEL é: ${this.NIP}.`)
+        console.log(`Seu HP é: ${this.PS}.`)
+        console.log(`Seu Sexo é: ${this.SDP}.`)
         console.log(`------------------------------------------------`)
     }
 }
-
-let P0 = new PokeCenter("Poochyena", "Dark", 2, "13/13", "M")
-let P1 = new PokeCenter("Zigzagoon", "Normal", 2, "13/13", "F")
-let P2 = new PokeCenter("Dragonite", "Dragon", 5, "25/25", "M")
-let P3 = new PokeCenter("Dragonite", "Dragon", 5, "24/24", "F")
-let P4 = new PokeCenter("Dragonite", "Dragon", 5, "24/24", "F")
-let P5 = new PokeCenter("Poochyena", "Dark", 3, "15/15", "F")
-let P6 = new PokeCenter("Wurmple", "Bug", 2, "7/14", "M")
-function PrintNotes(){
-P0.printPokeInfo()
-P1.printPokeInfo()
-P2.printPokeInfo()
-P3.printPokeInfo()
-P4.printPokeInfo()
-P5.printPokeInfo()
-P6.printPokeInfo()
+//--------------Variáveis com parametros e mais informações sobre os PKM---------------------------
+let P0 = new CentroPokemon("Poochyena", "Sombrio", 2, "13/13", "M")
+let P1 = new CentroPokemon("Zigzagoon", "Normal", 2, "13/13", "F")
+let P2 = new CentroPokemon("Dragonite", "Dragão", 5, "25/25", "M")
+let P3 = new CentroPokemon("Dragonite", "Dragão", 5, "24/24", "F")
+let P4 = new CentroPokemon("Dragonite", "Dragão", 5, "24/24", "F")
+let P5 = new CentroPokemon("Poochyena", "Sombrio", 3, "15/15", "F")
+let P6 = new CentroPokemon("Wurmple", "Inseto", 2, "7/14", "M")
+//---------------------Funçãoto para imprimir todas as notas---------------------------------------
+function ImprimirNotas(){
+P0.ImprimirInfo()
+P1.ImprimirInfo()
+P2.ImprimirInfo()
+P3.ImprimirInfo()
+P4.ImprimirInfo()
+P5.ImprimirInfo()
+P6.ImprimirInfo()
 }
-PrintNotes()
+//----------------------------Executar as Funções--------------------------------------------------
+ImprimirNotas()
+//------------------------------Fim do Código------------------------------------------------------
